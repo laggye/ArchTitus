9c1bee9911cb397f663c8b138b9ef1a62675146e
Alammsyah <81574768+laggye@users.noreply.github.com>
2021-11-22 09:37:49 +0530

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -27,22 +27,13 @@ PKGS=(
 'awesome-terminal-fonts'
 'brave-bin' # Brave Browser
 'dxvk-bin' # DXVK DirectX to Vulcan
-'github-desktop-bin' # Github Desktop sync
 'lightly-git'
 'lightlyshaders-git'
-'mangohud' # Gaming FPS Counter
 'mangohud-common'
 'nerd-fonts-fira-code'
-'nordic-darker-standard-buttons-theme'
-'nordic-darker-theme'
-'nordic-kde-git'
-'nordic-theme'
 'noto-fonts-emoji'
-'papirus-icon-theme'
 'plasma-pa'
 'ocs-url' # install packages from websites
-'sddm-nordic-theme-git'
-'snapper-gui-git'
 'ttf-droid'
 'ttf-hack'
 'ttf-meslo' # Nerdfont package
